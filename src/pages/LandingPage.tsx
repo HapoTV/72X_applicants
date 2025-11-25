d18cd77787ea876a4f63ef4aefa7d3210c6fc186
Asavela Mbengashe <mbengasheasavela808@gmail.com>
2025-11-25 14:52:06 +0200

    Hero Section approve

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -260,18 +260,18 @@ const LandingPage: React.FC = () => {
           className="relative bg-center bg-cover "
           style={{ backgroundAttachment: 'fixed'}}
         >
-          <div className="absolute inset-0 bg-blue-400" />
+          <div className="absolute inset-0 bg-white" />
           <div className="relative max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-32 md:py-40 min-h-[680px] lg:min-h-[580px]">
             <div className="flex justify-center items-center">
               <div className="text-blue text-center max-w-3xl">
-                <h1 className="text-5xl sm:text-6xl md:text-6xl font-extrabold leading-tight mb-4 text-white">
+                <h1 className="text-5xl sm:text-6xl md:text-6xl font-extrabold leading-tight mb-4 text-black">
                   {typedHeadline}
                   <span className="typing-caret align-middle" />
                 </h1>
-                <p className="text-4xl font-semibold mb-4 text-white">72X, the smart partner for your business growth.</p>
+                <p className="text-4xl font-semibold mb-4 text-black">72X, the smart partner for your business growth.</p>
                 <p className="mx-auto text-3xl text-black/90 mb-8">
                   Empowering South African entrepreneurs with AI-driven growth tools, local language support,
-                  interactive learning, and affordable business software tailored for <strong>townships and rural areas</strong>.
+                  interactive learning, and affordable business software tailored for <strong style={{color: '#0b76faff'}}>townships and rural areas</strong>.
                 </p>
 
                 <div className="flex flex-wrap justify-center gap-4">
